7acb0dd592fc8a14bb4a652e959d712e1f812116
Asemty <xcx701@gmail.com>
2017-08-30 18:27:08 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,133 +1,97 @@
-﻿function keyPress(evt){
-	switch(evt.keyCode) {
-        case 27:
-			pause=!pause;
-            break;
-		case 38:
-			if(body.length==0 || y-1!=body[0].y){
-				sy=-1;
-				sx=0;
-			}
-			move(0,-1);
-			pause=false;
-            break;
-		case 40:
-			if(body.length==0 || y+1!=body[0].y){
-				sy=1;
-				sx=0;
-			}
-			move(0,1);
-			pause=false;
-            break;
-		case 37:
-			if(body.length==0 || x-1!=body[0].x){
-				sy=0;
-				sx=-1;
-			}
-			move(-1,0);
-			pause=false;
-            break;
-		case 39:
-			if(body.length==0 || x+1!=body[0].x){
-				sy=0;
-				sx=1;
-			}
-			move(1,0);
-			pause=false;
-            break;
+﻿class Menu{
+	constructor(){
+		this.games=[];
+		this.games.unshift(new Snake());
+		this.games.unshift(new Snake());
+		this.games.unshift(new Snake());
+		this.currentMenuItem=0;
 	}
+	keyPress(evt){
+		switch(evt.keyCode) {
+			case 38:
+				this.up();
+				break;
+			case 40:
+				this.down();
+				break;
+			case 37:
+				this.left();
+				break;
+			case 39:
+				this.right();
+				break;
+			case 13:
+				this.choose();
+				break;
+		}
+	}
+	upd(){
+		contex.fillStyle = "#111111";
+		contex.fillRect(0, 0, canvas.width, canvas.height);
+		contex.fillStyle = "#eeeeaa";
+		
+		for(var i=0; i < this.games.length; i++){
+			contex.font = (i==this.currentMenuItem?"italic bold 35":"20")+"px arial,serif";
+			contex.fillText(i+1+". "+this.games[i].name,20,canvas.height/25*(i+1));
+
+		}
+	}
+	up(){
+		if(this.currentMenuItem == 0){
+			this.currentMenuItem = this.games.length - 1;
+		}else{
+			this.currentMenuItem--; 
+		}
+	}
+	down(){
+		if(this.currentMenuItem == this.games.length - 1){
+			this.currentMenuItem = 0;
+		}else{
+			this.currentMenuItem++; 
+		}
+	}
+	left(){}
+	right(){}
+	choose(){
+		curentPart=this.games[this.currentMenuItem];
+		curentPart.start();
+	}
+}
+
+function start(){
+	menu = new Menu();
+	curentPart=menu;
+}
+function keyPress(evt){
+	curentPart.keyPress(evt);
 }
 function update(){
-	contex.fillStyle="#111111";
-    contex.fillRect(0,0,canvas.width,canvas.height);
-	for(var i=0;i<width;i++){
-		for(var j=0;j<height;j++){
-			contex.fillStyle="#223322";
-			drawCell(i,j);
-		}
-	}
-	contex.fillStyle="#44ff44";
-    drawCell(x,y);
-	for(var i=0;i<body.length;i++){
-		contex.fillStyle="#225522";
-		drawCell(body[i].x,body[i].y)
-	}
-	contex.fillStyle="#ff4444";
-    drawCell(food.x,food.y);
-	if(!pause){
-		counter++;
-		if(counter>=maxCounter){
-			move(sx,sy);
-		}
-	}
+	curentPart.upd();
 }
-function drawCell(xx,yy){
-	contex.fillRect(cellSize*xx+offset,cellSize*yy+offset,cellSize-offset/2,cellSize-offset/2);
-}
-function move(xx,yy){
-	counter=0;
-	body.unshift({x: x,y: y});
-	if(food.x!=x+xx || food.y!=y+yy){
-		body.pop();
-	}else{
-		refreshFood();
+
+class Part{
+	constructor(name){
+		this.name=name;
 	}
-	x+=xx;
-	y+=yy;
-	if(x>=width) x = 0;
-	if(y>=height) y = 0;
-	if(x<0) x = width-1;
-	if(y<0) y = height-1;
-	for(var i=0;i<body.length;i++){
-		if(x==body[i].x && y==body[i].y){
-			tmp=body.length-i;
-			for(var j=0;j<tmp;j++){
-				body.pop();
-			}
-		}
-	}
-}
-function refreshFood(){
-	c=width*height-body.length-1;
-	if(c==0){
-		alert("Поздравляю, вы победили!");
-		start();
-	}else{
-		while(true){
-			food={x: Math.floor(Math.random()*width), y: Math.floor(Math.random()*height)};
-			for(var i=0;i<body.length;i++){
-				if(body[i].x==food.x && body[i].y==food.y){
-					break;
-				}
-			}
-			if(i==body.length){
-				if(food.x!=x || food.y!=y)return;
-			}
-		}
-	}
-}
-function start(){
-	pause=true;
-	body=[];
-	width=15;
-	height=20;
-	offset=4;
-	cellSize=(canvas.width-offset)/width;
-	x=Math.floor(Math.random()*width);
-	y=Math.floor(Math.random()*height);
-	sx=1;
-	sy=0;
-	counter=0;
-	maxCounter=10;
-	spd=prompt("Скорость игры (1-10)");
 	
-	food={x: Math.floor(Math.random()*width), y: Math.floor(Math.random()*height)};
-	while(true){
-		if(+spd>=1 && +spd<=10){
-			maxCounter=23-spd*2;
-			break;
+	keyPress(evt){
+		if(evt.keyCode == 27){//esc
+			this.pause=!this.pause;
+		}
+		if(evt.keyCode == 13 && this.pause){//enter
+			curentPart=menu;
+		}
+	}
+	upd(){
+		if(!this.pause){
+			this.update();
 		}else{
-			spd=prompt("Неверный ввод! Попробуйте ещё раз.\nСкорость игры (1-10)");
+			contex.fillStyle = "#111111";
+			contex.fillRect(canvas.width/4, canvas.height/4, canvas.width/2, canvas.height/2);
+			contex.fillStyle = "#eeeeaa";
+			contex.font = "italic bold 30px arial,serif";
+			contex.fillText("Выйти?",canvas.width/5*2,canvas.height/3+50);
+			contex.fillText("Enter-да, Esc-нет",canvas.width/4+25,canvas.height/3+120);
 		}
 	}
 }
---
+++ snake.js
@@ -0,0 +1,132 @@
+﻿class Snake extends Part{
+	constructor(){
+		super("Snake");
+	}
+	keyPress(evt){
+		super.keyPress(evt);
+		switch(evt.keyCode) {
+			case 38:
+				if(this.body.length==0 || this.y-1!=this.body[0].y){
+					this.sy=-1;
+					this.sx=0;
+				}
+				this.move(0,-1);
+				break;
+			case 40:
+				if(this.body.length==0 || this.y+1!=this.body[0].y){
+					this.sy=1;
+					this.sx=0;
+				}
+				this.move(0,1);
+				break;
+			case 37:
+				if(this.body.length==0 || this.x-1!=this.body[0].x){
+					this.sy=0;
+					this.sx=-1;
+				}
+				this.move(-1,0);
+				break;
+			case 39:
+				if(this.body.length==0 || this.x+1!=this.body[0].x){
+					this.sy=0;
+					this.sx=1;
+				}
+				this.move(1,0);
+				break;
+		}
+	}
+	start(){
+		this.pause=false;
+		this.body=[];
+		this.width=15;
+		this.height=20;
+		this.offset=4;
+		this.cellSize=(canvas.width-this.offset)/this.width;
+		this.x=Math.floor(Math.random()*this.width);
+		this.y=Math.floor(Math.random()*this.height);
+		this.sx=1;
+		this.sy=0;
+		this.counter=0;
+		this.maxCounter=10;
+		var spd=prompt("Скорость игры (1-10)");
+		
+		this.food={x: Math.floor(Math.random()*this.width), y: Math.floor(Math.random()*this.height)};
+		while(true){
+			if(+spd>=1 && +spd<=10){
+				this.maxCounter=23-spd*2;
+				break;
+			}else{
+				spd=prompt("Неверный ввод! Попробуйте ещё раз.\nСкорость игры (1-10)");
+			}
+		}
+	}
+	update(){
+		contex.fillStyle="#111111";
+		contex.fillRect(0,0,canvas.width,canvas.height);
+		for(var i=0;i<this.width;i++){
+			for(var j=0;j<this.height;j++){
+				contex.fillStyle="#223322";
+				this.drawCell(i,j);
+			}
+		}
+		contex.fillStyle="#44ff44";
+		this.drawCell(this.x,this.y);
+		for(var i=0;i<this.body.length;i++){
+			contex.fillStyle="#225522";
+			this.drawCell(this.body[i].x,this.body[i].y)
+		}
+		contex.fillStyle="#ff4444";
+		this.drawCell(this.food.x,this.food.y);
+		if(!this.pause){
+			this.counter++;
+			if(this.counter>=this.maxCounter){
+				this.move(this.sx,this.sy);
+			}
+		}
+	}
+	drawCell(xx,yy){
+		contex.fillRect(this.cellSize*xx+this.offset,this.cellSize*yy+this.offset,this.cellSize-this.offset/2,this.cellSize-this.offset/2);
+	}
+	move(xx,yy){
+		this.counter=0;
+		this.body.unshift({x: this.x,y: this.y});
+		if(this.food.x!=this.x+xx || this.food.y!=this.y+yy){
+			this.body.pop();
+		}else{
+			this.refreshFood();
+		}
+		this.x+=xx;
+		this.y+=yy;
+		if(this.x>=this.width) this.x = 0;
+		if(this.y>=this.height) this.y = 0;
+		if(this.x<0) this.x = this.width-1;
+		if(this.y<0) this.y = this.height-1;
+		for(var i=0;i<this.body.length;i++){
+			if(this.x==this.body[i].x && this.y==this.body[i].y){
+				var tmp=this.body.length-i;
+				for(var j=0;j<tmp;j++){
+					this.body.pop();
+				}
+			}
+		}
+	}
+	refreshFood(){
+		var c=this.width*this.height-this.body.length-1;
+		if(c==0){
+			alert("Поздравляю, вы победили!");
+			this.start();
+		}else{
+			while(true){
+				this.food={x: Math.floor(Math.random()*this.width), y: Math.floor(Math.random()*this.height)};
+				for(var i=0;i<this.body.length;i++){
+					if(this.body[i].x==this.food.x && this.body[i].y==this.food.y){
+						break;
+					}
+				}
+				if(i==this.body.length){
+					if(this.food.x!=this.x || this.food.y!=this.y)return;
+				}
+			}
+		}
+	}	
+}
