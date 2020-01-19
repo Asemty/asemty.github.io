964f335f8d31cd6615c463bf6607c66383d613ea
Asemty <xcx701@gmail.com>
2020-01-19 23:40:20 +0500

    Add files via upload

--- games.js
+++ games.js
@@ -3,17 +3,19 @@
 })(
 	[
 		[
-			{name: "9999999 in 1", discription: "Five complited games", imgSrc: "img/tetris.png", htmlSrc: "games/TetriScript/game.html"}
+			
 		],
 		[
-			{name: "Jafer Strike", discription: "Platformer with one boss", imgSrc: "img/Jafer Strike.png", htmlSrc: "games/Jafer Strike/game.html"},
-			{name: "Laser rod", discription: "Top-Down engineering survival with energy manipulation ", imgSrc: "img/Laser rod.png", htmlSrc: "games/Laser rod/game.html"},
-			{name: "RPG test battle", discription: "Test rytm-battle mechanic", imgSrc: "img/testbattle.png", htmlSrc: "games/Test RPG fight/game.html"}
+			{name: "9999999 in 1", discription: "Five complited games", imgSrc: "img/tetris.png", htmlSrc: "games/TetriScript/game.html"},
+			{name: "Jafer Strike", discription: "Platformer with one boss", imgSrc: "img/Jafer Strike.png", htmlSrc: "games/Jafer Strike/game.html"}
+			
 		],
 		[
-			{name: "Drawer", discription: "Test polar coordinate system", imgSrc: "img/drawer.png", htmlSrc: "games/DrawerCirc/game.html"},
+			{name: "RPG test battle", discription: "Rytm-battle mechanic", imgSrc: "img/testbattle.png", htmlSrc: "games/Test RPG fight/game.html"},
 			{name: "Pather", discription: "Pathern editor", imgSrc: "img/pather.png", htmlSrc: "games/Pather/game.html"},
-			{name: "Major First Minute", discription: "RTS", imgSrc: "img/mfm.png", htmlSrc: "games/Major First Minute/game.html"}
+			{name: "Kars", discription: "Ride & Gun", imgSrc: "img/kars.png", htmlSrc: "games/Kars/game.html"},
+			{name: "Last Mo(v)ment", discription: "Top down", imgSrc: "img/lmv.png", htmlSrc: "games/Last Mo(v)ment/game.html"},
+			{name: "Laser rod", discription: "Top-Down engineering survival with energy manipulation ", imgSrc: "img/Laser rod.png", htmlSrc: "games/Laser rod/game.html"}
 		]
 	]
 );
