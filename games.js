0171eba46a5a35b8e67f7b02ae15ca7e913edf5a
Asemty <xcx701@gmail.com>
2020-07-22 14:59:42 +0500

    Update games.js

--- games.js
+++ games.js
@@ -3,20 +3,18 @@
 })(
 	[
 		[
-			
+			{name: "9999999 in 1", discription: "Четыре завершённых игры, один интересный тест", imgSrc: "img/tetris.png", htmlSrc: "games/TetriScript/game.html"},
+			{name: "Pather", discription: "Графический редактор патернов", imgSrc: "img/pather.png", htmlSrc: "games/Pather/game.html"},
+			{name: "Jafer Strike", discription: "Платформер с 7 видами оружия и 1 боссом", imgSrc: "img/Jafer Strike.png", htmlSrc: "games/Jafer Strike/game.html"}
 		],
 		[
-			{name: "9999999 in 1", discription: "Five complited games", imgSrc: "img/tetris.png", htmlSrc: "games/TetriScript/game.html"},
-			{name: "Jafer Strike", discription: "Platformer with one boss", imgSrc: "img/Jafer Strike.png", htmlSrc: "games/Jafer Strike/game.html"}
-			
+			{name: "RPG test battle", discription: "Возможная механика для битв", imgSrc: "img/testbattle.png", htmlSrc: "games/Test RPG fight/game.html"},
+			{name: "Last Mo(v)ment", discription: "TDS с остановленным временем", imgSrc: "img/lmv.png", htmlSrc: "games/Last Mo(v)ment/game.html"}
 		],
 		[
-			{name: "RPG test battle", discription: "Rytm-battle mechanic", imgSrc: "img/testbattle.png", htmlSrc: "games/Test RPG fight/game.html"},
-			{name: "Pather", discription: "Pathern editor", imgSrc: "img/pather.png", htmlSrc: "games/Pather/game.html"},
-			{name: "Kars", discription: "Ride & Gun", imgSrc: "img/kars.png", htmlSrc: "games/Kars/game.html"},
-			{name: "Last Mo(v)ment", discription: "Top down", imgSrc: "img/lmv.png", htmlSrc: "games/Last Mo(v)ment/game.html"},
-			{name: "Laser rod", discription: "Top-Down engineering survival with energy manipulation ", imgSrc: "img/Laser rod.png", htmlSrc: "games/Laser rod/game.html"},
-			{name: "Difficult calc", discription: "Калькулятор для расчёта времени изучения призываемого предмета (DnD) ", imgSrc: "img/difcalc.png", htmlSrc: "games/difcalc/game.html"}
+			{name: "Difficult calc", discription: "Калькулятор для расчёта времени изучения призываемого предмета (DnD) ", imgSrc: "img/difcalc.png", htmlSrc: "games/difcalc/game.html"},
+			{name: "Kars", discription: "Прототип игры в жанре Ride & Gun", imgSrc: "img/kars.png", htmlSrc: "games/Kars/game.html"},
+			{name: "Laser rod", discription: "Прототип игры с манипуляцией энергии", imgSrc: "img/Laser rod.png", htmlSrc: "games/Laser rod/game.html"}
 		]
 	]
 );
